4cee776dad62c4ddd1c8d9f046abe8297c727374
icarvaja <icarvaja@everis.com>
2019-07-05 14:58:49 -0400

    se agrego websocket para comunicar el chat

--- Front/js/app.js
+++ Front/js/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var user_name
+    var final_connection
 
     $("#form_registro").submit(function(e){
         e.preventDefault();
@@ -27,6 +29,27 @@ $(document).ready(function(){
     }   
 
     function createConexion(){
-        
+        $("#container").hide();
+        $("#container_chat").show();
+
+        var conexion = new WebSocket("ws://localhost:4545/chat/" + user_name);
+        final_connection = conexion;
+        conexion.onopen = function(ressponse){
+            conexion.onmessage = function(ressponse){
+                console.log("Nos envio algo")
+                console.log(ressponse.data);
+                val = $("#chat_area").val();
+                $("#chat_area").val(val + "\n" + ressponse.data)
+
+                
+            }
+        }
     }
+
+    $("#form_message").on("submit", function(e){
+        e.preventDefault();
+        mensaje = $("#msg").val();
+        final_connection.send(mensaje);
+        $("#msg").val("")
+    })
 })
--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 type Response struct {
@@ -22,12 +23,24 @@ var Users = struct {
 }{m: make(map[string]User)}
 
 type User struct {
-	userName string `json: "name"`
+	userName  string `json: "name"`
+	Websocket *websocket.Conn
+}
+
+func createUser(userName string, ws *websocket.Conn) User {
+	return User{userName, ws}
+}
+
+func AddUser(user User) {
+	Users.Lock()
+	defer Users.Unlock()
+	Users.m[user.userName] = user
 }
 
 func hola(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hola Mundo!!!!"))
 }
+
 func holaJson(w http.ResponseWriter, r *http.Request) {
 	response := CreateResponse("Hola desde Json", 200, true)
 	json.NewEncoder(w).Encode(response)
@@ -62,6 +75,54 @@ func validate(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func removeUser(userName string) {
+	Users.Lock()
+	defer Users.Unlock()
+	delete(Users.m, userName)
+}
+
+func sendMessage(typeMessage int, message []byte) {
+	Users.RLock()
+	defer Users.RUnlock()
+
+	for _, user := range Users.m {
+		if err := user.Websocket.WriteMessage(typeMessage, message); err != nil {
+			return
+		}
+	}
+}
+
+func toArrayByte(value string) []byte {
+	return []byte(value)
+}
+
+func concatMessage(userName string, array []byte) string {
+	return userName + " : " + string(array[:])
+}
+
+func webSocket(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userName := vars["user_name"]
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	currentUser := createUser(userName, ws)
+	AddUser(currentUser)
+	log.Println("Nuevo Ususario agregado")
+
+	for {
+		typeMessage, message, err := ws.ReadMessage()
+		if err != nil {
+			removeUser(userName)
+			return
+		}
+		finalMessage := concatMessage(userName, message)
+		sendMessage(typeMessage, toArrayByte(finalMessage))
+	}
+}
+
 func CreateResponse(message string, status int, isValid bool) Response {
 	return Response{message, status, isValid}
 }
@@ -74,6 +135,7 @@ func main() {
 	mux.HandleFunc("/json", holaJson).Methods("GET")
 	mux.HandleFunc("/", loadHtml).Methods("GET")
 	mux.HandleFunc("/validate", validate).Methods("POST")
+	mux.HandleFunc("/chat/{user_name}", webSocket).Methods("GET")
 	http.Handle("/", mux)
 	http.Handle("/css/", http.StripPrefix("/css/", cssHandle))
 	http.Handle("/js/", http.StripPrefix("/js/", jsHandle))
